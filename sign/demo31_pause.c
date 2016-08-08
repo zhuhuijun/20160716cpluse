de340c4a3db66a8a464ff0947b583043d039e998
zhuhuijun <zhuhjtcx@qq.com>
2016-08-09 07:12:17 +0800

    signal

--- sign/demo31_pause.c
+++ sign/demo31_pause.c
@@ -6,8 +6,13 @@
 #include <sys/wait.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-
+int func1(int a,int b){
+	return a+b;
+}
 int main(){
+	int (*p)(int,int);
+	p=func1;
+	printf("p=%d\n",p(4,6) );
 	pause();
 	printf("end\n");
 	return 0;
