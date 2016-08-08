b92c71d733d595abc4c95c11084efebe3d49d632
zhuhuijun <zhuhjtcx@qq.com>
2016-08-09 07:20:50 +0800

    sigint

--- sign/demo31_pause.c
+++ sign/demo31_pause.c
@@ -6,14 +6,25 @@
 #include <sys/wait.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <signal.h>
 int func1(int a,int b){
 	return a+b;
+}
+void signalctrl(int sign){
+	switch(sign){
+		case SIGINT:
+		printf("Sigint\n");
+	}
 }
 int main(){
 	int (*p)(int,int);
 	p=func1;
 	printf("p=%d\n",p(4,6) );
-	pause();
+	signal(SIGINT,signalctrl);
 	printf("end\n");
+	while(1){
+		printf("hello\n");
+		sleep(1);
+	}
 	return 0;
 }
