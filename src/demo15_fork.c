aace914aef1154ba19f2f694ff051170ee12fe89
zhuhjtcx@qq.com <zzbj891016>
2016-08-08 12:56:18 +0800

    gg

--- src/demo15_fork.c
+++ src/demo15_fork.c
@@ -1,15 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 int main(int argc, char const *argv[])
 {
-	pid_t children=fork();
-	if(children==-1){
-		printf("call fork error\n", );
+	pid_t child=fork();
+	if(child==-1){
+		printf("call fork error\n" );
 		return -1;
 	}
-	if (children==1)
+	if (child==1)
 	{
 		printf("is children \n");
 	}else{
-		printf("child pid is%d \n",children);
+		printf("child pid is  %d \n",child);
 	}
 	return 0;
 }
