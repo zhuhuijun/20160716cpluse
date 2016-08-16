63c4e5b7a4b063af55444a8b65e726b9d95a3e01
zhuhjtcx@qq.com <zzbj891016>
2016-08-16 09:49:20 +0800

    commit

--- conn/src/demo37_join.c
+++ conn/src/demo37_join.c
@@ -25,26 +25,35 @@
  }
 
  void *func2(void *arg){
- 	printf("func2 start\n");
+ 	int *p=(int *) arg;
+ 	int i=*p;
+ 	free(p);
+ 	printf("func%d start\n",i);
+ 	//static int a=0;//static是全局的
  	int a=0;
- 	for(;a<10;a++){
- 		printf("func2   start\n");
+ 	for(;a<i;a++){
+ 		printf("func%d  start %d\n",i,a);
  		sleep(1);
  	}
- 	printf("func2 end\n");
+ 	printf("func%d end\n",i);
  	return NULL;//代表线程的结束
  }
 
  int main(void)
  {
  	pthread_t thrd,thrd2;
- 	if(pthread_create(&thrd,NULL,func1,NULL)!=0){
+ 	int *i1=malloc(sizeof(int));
+ 	int *i2=malloc(sizeof(int));
+ 	*i1=10;
+ 	*i2=5;
+ 	if(pthread_create(&thrd,NULL,func2,i1)!=0){
  		printf("errno %s\n",strerror(errno) );
  	}
- 	if(pthread_create(&thrd2,NULL,func2,NULL)!=0){
+ 	if(pthread_create(&thrd2,NULL,func2,i2)!=0){
  		printf("errno %s\n",strerror(errno) );
  	}
  	pthread_join(thrd2,NULL);//主线程挂起等待thrd2退出
+ 	pthread_join(thrd,NULL);//主线程挂起等待thrd2退出
  	printf("main end\n");
  	
  	return EXIT_SUCCESS;
