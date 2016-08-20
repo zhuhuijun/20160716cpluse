2d1a78939b24ebc0339ceedcc6f1f9fa73bd9c83
zhuhuijun <zhuhjtcx@qq.com>
2016-08-20 08:16:04 +0800

    upper

---
+++ conn/src/libso/upper.c
@@ -0,0 +1,14 @@
+#include <string.h>
+//大小写的转换
+void upper(const char *src,char *desc)
+{
+	int len=strlen(src);
+	int i=0;
+	for(;i<len;i++){
+		if((src[i]>='a')&&(src[i]<='z')){
+			desc[i]=src[i]-0x20;
+		}else{
+			desc[i]=src[i];
+		}
+	}
+}
