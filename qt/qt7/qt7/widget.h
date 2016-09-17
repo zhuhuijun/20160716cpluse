8a698f8ea4ccad3891f77b150fee68deb2e29edb
zhuhuijun <zhuhjtcx@qq.com>
2016-09-17 20:57:00 +0800

    ss

--- qt/qt7/qt7/widget.h
+++ qt/qt7/qt7/widget.h
@@ -2,6 +2,9 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QLabel>
+#include <QPushButton>
+#include <QTimer>
 
 class Widget : public QWidget
 {
@@ -10,6 +13,14 @@ class Widget : public QWidget
 public:
     Widget(QWidget *parent = 0);
     ~Widget();
+private:
+    QLabel *label1;
+    QPushButton *btn1;
+    QTimer *tm;
+    QStringList strlist;
+private slots:
+    void on_clicked();
+    void on_timer();
 };
 
 #endif // WIDGET_H
