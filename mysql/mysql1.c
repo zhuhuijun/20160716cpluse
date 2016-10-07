f87ac78bba61c712cdf1c7e0bd23f36fa8e4a101
zhuhuijun <zhuhjtcx@qq.com>
2016-10-08 07:20:13 +0800

    mysql

---
+++ mysql/mysql1.c
@@ -0,0 +1,34 @@
+/*
+/*
+ ============================================================================
+ Name        : mysql1.c
+ Author      : zhuhj
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <termios.h>
+#include <mysql/mysql.h>
+
+#define BUFSIZE 1024
+
+int main(int arg, char *args[])
+{
+	MYSQL mysql,*connection;
+	mysql_init(&mysql);//mysql内部初始化一个TCPsocket
+	connection=mysql_real_connect(&mysql,"localhost","dbuser1","dbuser1","db1",0,0,0);
+	if(connection==NULL)
+	{
+		printf("connect error\n");
+		return -1;
+	}
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
---
+++ mysql/readme.md
@@ -0,0 +1,2 @@
+# 1.#include<mysql/mysql.h>
+# 2.makefile 中链接mysql的动态库
