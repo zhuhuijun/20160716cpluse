d3cb4cdf0d55eac77dd38575b78191194b6f9021
zhuhuijun <zhuhjtcx@qq.com>
2016-10-09 07:19:45 +0800

    mysql

--- mysql/mysql1.c
+++ mysql/mysql1.c
@@ -29,8 +29,26 @@ int main(int arg, char *args[])
 		printf(mysql_error(&mysql));
 		return -1;
 	}
+	int ret1=mysql_query(connection,"SET NAMES utf8;");
+	if(ret1!=0)
+	{
+		printf("set character error\n");
+	}	
+	char name[BUFSIZE];
+	memset(name,0,sizeof(name));
+	read(STDIN_FILENO,name,sizeof(name));
+	name[strlen(name)-1]=0;//最后的换行去掉
+	char SQL[BUFSIZE];
+	memset(SQL,0,sizeof(SQL));
+	sprintf(SQL,"delete from table1 where name='%s';",name);
+
+	int ret=mysql_query(connection,SQL);
+	if(ret!=0)
+	{
+		printf("query error,%s\n",mysql_error(&mysql) );
+	}
 	//关闭mysql
 	mysql_close(connection);
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	puts("!!!Hello World!!!"); 
 	return EXIT_SUCCESS;
 }
--- mysql/readme.md
+++ mysql/readme.md
@@ -1,2 +1,3 @@
 # 1.include`<mysql/mysql.h>`
 # 2.makefile 中链接mysql的动态库`$(CC) -o $(EXEC1) $(OBJS1) -lmysqlclient`
+# 3.设置链接的字符是utf8`int ret1=mysql_query(connection,"SET NAMES utf8;");`
